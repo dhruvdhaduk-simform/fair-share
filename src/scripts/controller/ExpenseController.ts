391a631972641feace86ad05cd9dcac36b5348d9
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-04 15:50:14 +0530

    feature: Add logic for creating participants and expenses in Expense Controller

--- src/scripts/controller/ExpenseController.ts
+++ src/scripts/controller/ExpenseController.ts
@@ -7,9 +7,69 @@ export class ExpenseController {
     constructor() {
         this.#participants = [];
         this.#expenses = [];
+    }
 
-        console.log(this.#participants);
-        console.log(this.#expenses);
+    // Return a participant by name. Or create new one if doesn't exist.
+    getParticipant(name: string): Participant {
+        name = name.trim();
+
+        const participantIndex = this.#participants.findIndex(
+            (p) => p.name === name
+        );
+
+        if (participantIndex !== -1) {
+            return this.#participants[participantIndex];
+        }
+
+        const participant: Participant = {
+            id: crypto.randomUUID(),
+            name,
+            balance: 0,
+        };
+
+        this.#participants.push(participant);
+
+        return participant;
+    }
+
+    // Add a new expenst.
+    addNewExpense(
+        title: string,
+        description: string,
+        amount: number,
+        participantNames: Array<string>,
+        paidByPersonName: string
+    ) {
+        if (!participantNames.includes(paidByPersonName)) {
+            throw new Error('Person who paid is not included in participants.');
+        }
+
+        // Get participants from their names.
+        const participants: Array<Participant> = participantNames.map((p) => {
+            return this.getParticipant(p);
+        });
+
+        const paidBy = this.getParticipant(paidByPersonName);
+        const notSettled = participants.filter((p) => p.name !== paidBy.name);
+        const actualAmount = amount - amount / participants.length;
+
+        // Update the balance of participants according to this expense.
+        paidBy.balance += actualAmount;
+        notSettled.forEach((p) => {
+            p.balance -= actualAmount / notSettled.length;
+        });
+
+        const expense: Expense = {
+            id: crypto.randomUUID(),
+            title,
+            description,
+            amount: actualAmount,
+            paidBy,
+            settled: [],
+            notSettled,
+        };
+
+        this.#expenses.push(expense);
     }
 
     getAllParticipants() {
