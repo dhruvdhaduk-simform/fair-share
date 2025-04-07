4e35a5a7e6b01ba186631bce63ee5a5f3c95a660
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-07 17:00:26 +0530

    feature: Add submit handler to Add New Expense form

--- src/scripts/controller/ExpenseController.ts
+++ src/scripts/controller/ExpenseController.ts
@@ -1,12 +1,26 @@
 import type { Participant, Expense } from '../model/interfaces.ts';
+import { ExpensesView } from '../view/ExpensesView.ts';
 
 export class ExpenseController {
     #participants: Array<Participant>;
     #expenses: Array<Expense>;
+    #addExpenseForm: HTMLFormElement;
+    #view: ExpensesView;
 
     constructor() {
         this.#participants = [];
         this.#expenses = [];
+
+        this.#view = new ExpensesView();
+
+        this.#addExpenseForm = document.querySelector(
+            '#add-expense-form'
+        ) as HTMLFormElement;
+
+        this.#addExpenseForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            this.handleAddExpenseFormSubmit();
+        });
     }
 
     // Return a participant by name. Or create new one if doesn't exist.
@@ -91,6 +105,8 @@ export class ExpenseController {
         };
 
         this.#expenses.push(expense);
+
+        this.#view.renderExpenses(this.#expenses);
     }
 
     // Settle an expense for a particular participant.
@@ -128,5 +144,51 @@ export class ExpenseController {
 
     getAllExpenses() {
         return this.#expenses;
+    }
+
+    // Submit handler for Add Expense Form.
+    handleAddExpenseFormSubmit() {
+        // Utility function to get value of an input field.
+        const getValue = (selector: string) =>
+            (
+                this.#addExpenseForm.querySelector(selector) as HTMLInputElement
+            )?.value.trim() || '';
+
+        const title = getValue('#title');
+        const description = getValue('#description');
+        const amount = Number(getValue('#amount'));
+        const paidByPersonName = getValue('#paid-by');
+
+        // Parse all the participant names as a string array.
+        const participantNames: Array<string> = Array.from(
+            this.#addExpenseForm.querySelector('.participants')?.children ?? []
+        )
+            .map((item) => {
+                return item.textContent?.trim();
+            })
+            .filter((participant) => typeof participant === 'string');
+
+        try {
+            this.addNewExpense(
+                title,
+                description,
+                amount,
+                participantNames,
+                paidByPersonName
+            );
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                alert(err.message);
+                console.error(err.message);
+            } else {
+                console.error(err);
+            }
+        }
+
+        // Close the popup after submission.
+        const popup = this.#addExpenseForm.closest('div[popover]');
+        if (popup instanceof HTMLDivElement) {
+            popup.hidePopover();
+        }
     }
 }
